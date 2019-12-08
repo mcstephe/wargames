825e88fb0bd38660f2c73c995bde239c29d1b3be
mcstephe <mcstephe@gmail.com>
2019-12-07 20:51:30 -0500

    AoC Day1-2 Update

---
+++ AdventOfCode/2019/1/Day1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            int totalMassPart1 = 0;
+            int totalMassPart2 = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\mcstephe\Documents\GitHub\wargames\AdventOfCode\2019\1\input.txt");
+
+            foreach (string line in lines)
+            {
+                totalMassPart1 += GetFuelReqPart1(Convert.ToInt32(line));
+                totalMassPart2 += GetFuelReqPart2(Convert.ToInt32(line));
+            }
+
+            //Correct answers are 3515171 and 5269882.
+            Console.WriteLine("Fuel Required \n Part 1: {0} Part 2: {1}", totalMassPart1, totalMassPart2); 
+        }
+
+        static int GetFuelReqPart1(int mass)
+        {
+            //Do not need to round down. Casting as int will drop off.
+            int fuel = mass / 3; 
+            fuel -= 2; 
+
+            return fuel;
+        }
+
+        static int GetFuelReqPart2(int mass) 
+        {
+            
+            int fuel = mass / 3; 
+            fuel -= 2;
+
+            //8 is highest value that is non negative after conversion that doesn't require additional recursion
+            if (fuel <= 8 ) 
+            {
+                return fuel;
+            }
+
+            //Use Recursion to solve additional fuel requirements.
+            return fuel + GetFuelReqPart2(fuel);
+        }
+
+    }
+}
---
+++ AdventOfCode/2019/2/Day2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            string stringCode = System.IO.File.ReadAllText(@"C:\Users\mcstephe\source\repos\AdventOfCode\AdventOfCode\Day_2_Input.txt");
+
+            int pointer;
+            bool isRunning;
+            int[] intCode = {0};  
+
+            for (int i = 0; i <= 99; i++)
+            {
+                for (int j = 0; j <= 99; j++)
+                {
+                    //Reset memory
+                    pointer = 0;
+                    intCode = Array.ConvertAll<string, int>(stringCode.Split(','), Convert.ToInt32);
+
+                    intCode[1] = i;
+                    intCode[2] = j;
+                   
+                    int caseSwitch = intCode[pointer];
+                    isRunning = true;
+
+                    while (isRunning)
+                    {
+                        switch (caseSwitch)
+                        {
+                            case 1:
+                                intCode[intCode[pointer + 3]] = intCode[intCode[pointer + 1]] + intCode[intCode[pointer + 2]];
+                                pointer += 4;
+                                caseSwitch = intCode[pointer];
+                                break;
+                            case 2:
+                                intCode[intCode[pointer + 3]] = intCode[intCode[pointer + 1]] * intCode[intCode[pointer + 2]];
+                                pointer += 4;
+                                caseSwitch = intCode[pointer];
+                                break;
+                            case 99:
+                                isRunning = false;
+                                break;
+                            default:
+                                //Console.WriteLine("Ignore invalid op codes");
+                                break;
+                        }
+                    }
+
+                    if (intCode[0] == 19690720)
+                    {
+                        Console.Write("FOUND 19690720 OUTPUT: {0} {1}", intCode[1], intCode[2]);
+                        break;
+                    }
+                }
+                if (intCode[0] == 19690720)
+                {
+                    Console.Write("FOUND 19690720 OUTPUT: {0} {1}", intCode[1], intCode[2]);  //Correct answer is 20 03
+                    break;
+                }
+
+            }
+                
+        }
+    }
+}
